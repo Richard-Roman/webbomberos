cb33e4e553be1558aa0eab8349081290c0db44fb
JhonCatNigggger <116854765+JhordanTejada@users.noreply.github.com>
2025-04-21 12:03:26 -0500

    Update SecurityConfig.java
    Endpoint del modulo Seguridad accecibels para usuarios con rol ADMIN Y DEVELOPER

--- src/main/java/bomberos/tarapoto/webbomberos/config/SecurityConfig.java
+++ src/main/java/bomberos/tarapoto/webbomberos/config/SecurityConfig.java
@@ -1,34 +1,19 @@
 package bomberos.tarapoto.webbomberos.config;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import bomberos.tarapoto.webbomberos.Jwt.JwtAuthenticationFilter;
-import bomberos.tarapoto.webbomberos.service.UserDetailServiceImpl;
-import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -36,34 +21,12 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 
-
 public class SecurityConfig {
 
     
     @Autowired
     AuthenticationConfiguration authenticationConfiguration;
 
-    /* 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-            .csrf(csrf -> csrf.disable())
-            .httpBasic(Customizer.withDefaults())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(http -> {
-                //Configurate endpoint publicos
-                http.requestMatchers(HttpMethod.GET,"/webbomberos/intranet/login").permitAll();
-                
-                //Configurate endpoint privados                
-                http.requestMatchers(HttpMethod.GET,"/webbomberos/intranet/Usuarios/Visualizar_Usuarios").hasAuthority("CREATE");
-
-                //Configurate endpoint publicos
-
-                http.anyRequest().denyAll();
-            })
-            .build();
-    }
-    */
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authProvider;
 
@@ -85,9 +48,11 @@ public class SecurityConfig {
                 // Configurar los endpoints privados
                 authRequest.requestMatchers("/webbomberos/intranet/main").authenticated();
                 authRequest.requestMatchers("/webbomberos/intranet/perfil").hasAnyRole("USER");
-                authRequest.requestMatchers("/webbomberos/intranet/usuarios/verusuarios").hasAnyRole("ADMIN", "DEVELOPER");
-                authRequest.requestMatchers("/webbomberos/intranet/usuarios/ucreate").hasAnyRole("ADMIN", "DEVELOPER");
-                authRequest.requestMatchers("/webbomberos/intranet/usuarios/ueditar/{id}").hasAnyRole("ADMIN", "DEVELOPER");
+                authRequest.requestMatchers("/webbomberos/intranet/Usuarios/vusers").hasAnyRole("ADMIN", "DEVELOPER");
+                authRequest.requestMatchers("/webbomberos/intranet/Usuarios/registraruser").hasAnyRole("ADMIN", "DEVELOPER");
+                authRequest.requestMatchers("/webbomberos/intranet/Usuarios/visualizaruser/{id}").hasAnyRole("ADMIN", "DEVELOPER");
+                authRequest.requestMatchers("/webbomberos/intranet/Usuarios/editaruser/{id}").hasAnyRole("ADMIN", "DEVELOPER");
+                authRequest.requestMatchers("/webbomberos/intranet/Usuarios/formusers").hasAnyRole("ADMIN", "DEVELOPER");
 
                 
                 // Configurar el resto de endpoint - NO ESPECIFICADOS
@@ -95,26 +60,6 @@ public class SecurityConfig {
             })
             .build();
     }
-
- /* 
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(UserDetailServiceImpl userDetailService){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailService);
-        return provider;
-    }
-
-
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
-*/
     
 }
 
